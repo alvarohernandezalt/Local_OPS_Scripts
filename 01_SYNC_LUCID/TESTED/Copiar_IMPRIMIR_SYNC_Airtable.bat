4cafea079b09aa593f1a6eb5296d7e54f205c06c
alvarohernandezalt <107269919+alvarohernandezalt@users.noreply.github.com>
2023-07-03 16:05:57 +0200

    Update Copiar_IMPRIMIR_SYNC_Airtable.bat

--- 01_SYNC_LUCID/TESTED/Copiar_IMPRIMIR_SYNC_Airtable.bat
+++ 01_SYNC_LUCID/TESTED/Copiar_IMPRIMIR_SYNC_Airtable.bat
@@ -1,6 +1,6 @@
 @echo off
-set "origen=L:\ES_DPP\08_Dossier\04_DPP_Informes_PDF\01_IMPRIMIR"
-set "destino=L:\ES_BI\z_NO_TOCAR\04_TRANSFER"
+set "origen=ORIGIN/FOLDER"
+set "destino=DESTINY/FOLDER"
 
 for /R "%origen%" %%F in (*.pdf) do (
     copy "%%F" "%destino%\"
